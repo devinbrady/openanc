d0c2737471f1252c8197f5df08829de2e2634155
Devin Brady <devinbrady@gmail.com>
2020-07-16 16:27:08 -0400

    Homepage title

--- build_site.py
+++ build_site.py
@@ -6,11 +6,11 @@ from scripts.refresh_data import RefreshData
 from scripts.build_index import BuildIndex
 from scripts.build_districts import BuildDistricts
 
-# r = RefreshData()
-# r.run()
+r = RefreshData()
+r.run()
 
-# bi = BuildIndex()
-# bi.run()
+bi = BuildIndex()
+bi.run()
 
 bd = BuildDistricts()
 bd.run()
