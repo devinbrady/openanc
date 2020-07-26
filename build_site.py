f3666c8098113b070bb63ac36992175af414c87a
Devin Brady <devinbrady@gmail.com>
2020-07-26 12:30:30 -0400

    Use command-line arguments

--- build_site.py
+++ build_site.py
@@ -2,19 +2,35 @@
 All steps necessary to build OpenANC pages
 """
 
+import argparse
+
 from scripts.refresh_data import RefreshData
 from scripts.build_index import BuildIndex
 from scripts.build_districts import BuildDistricts
 from scripts.build_ancs import BuildANCs
 
-r = RefreshData()
-r.run()
 
-bi = BuildIndex()
-bi.run()
+parser = argparse.ArgumentParser()
+parser.add_argument('-r', '--refresh-data', action='store_true', help='Refresh local CSVs from Google Sheets')
+parser.add_argument('-i', '--build-index', action='store_true', help='Build index and other top-level pages')
+parser.add_argument('-a', '--build-ancs', action='store_true', help='Build page for each ANC')
+parser.add_argument('-d', '--build-districts', action='store_true', help='Build page for each SMD')
 
-ba = BuildANCs()
-ba.run()
+args = parser.parse_args()
 
-bd = BuildDistricts()
-bd.run()
+
+if args.refresh_data:
+    r = RefreshData()
+    r.run()
+
+if args.build_index:
+    bi = BuildIndex()
+    bi.run()
+
+if args.build_ancs:
+    ba = BuildANCs()
+    ba.run()
+
+if args.build_districts:
+    bd = BuildDistricts()
+    bd.run()
