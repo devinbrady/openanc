b155f646a12b4d217f3d9cfb775157a56d59332f
Devin Brady <devinbrady@gmail.com>
2020-07-27 18:21:04 -0400

    Add counts page

--- scripts/counts.py
+++ scripts/counts.py
@@ -1,34 +1,105 @@
 # counts.py
 
 import pandas as pd
-from refresh_data import RefreshData
+from scripts.refresh_data import RefreshData
 
 
-def smd_candidate_count(groupby_field):
-    """
-    Count the number of SMDs and number of SMDs with candidates in each ANC or ward
-    """
+class Counts():
 
-    rd = RefreshData()
-    df = rd.assemble_smd_info()
+    def smd_candidate_count(self, groupby_field, bar_color):
+        """
+        Count the number of SMDs and number of SMDs with candidates in each ANC or ward
+        """
 
-    df['has_candidate'] = df['number_of_candidates'] > 0
+        rd = RefreshData()
+        df = rd.assemble_smd_info()
 
-    districts = pd.read_csv('data/districts.csv')
-    dfd = pd.merge(df, districts, how='inner', on='smd_id')
+        df['has_candidate'] = df['number_of_candidates'] > 0
 
-    smd_count = dfd.groupby(groupby_field).agg(
-        num_smds = ('smd_id', 'count')
-        , has_candidate = ('has_candidate', 'sum')
-        )
+        districts = pd.read_csv('data/districts.csv')
+        dfd = pd.merge(df, districts, how='inner', on='smd_id')
 
-    smd_count['percentage_with_candidate'] = smd_count['has_candidate'] / smd_count['num_smds']
+        if groupby_field == 'dc':
 
-    print(smd_count)
-    # smd_count.to_csv(groupby_field + '.csv')
+            dfd['State'] = 'DC'
+
+            smd_count = dfd.groupby('State').agg(
+                num_smds = ('smd_id', 'count')
+                , has_candidate = ('has_candidate', 'sum')
+                )
+
+        else:
+
+            smd_count = dfd.groupby(groupby_field).agg(
+                num_smds = ('smd_id', 'count')
+                , has_candidate = ('has_candidate', 'sum')
+                )
 
 
-if __name__ == '__main__':
+        smd_count['Percentage with Candidate'] = smd_count['has_candidate'] / smd_count['num_smds']
 
-    smd_candidate_count('anc_id')
-    smd_candidate_count('ward')
+        smd_count = smd_count.reset_index()
+        smd_count.rename(columns={
+            'ward': 'Ward'
+            , 'anc_id': 'ANC'
+            , 'num_smds': 'Count of SMDs'
+            , 'has_candidate': 'Has Candidate'
+            }, inplace=True
+            )
+
+        
+        smd_html = (
+            smd_count.style
+                .set_properties(**{
+                    'border-color': 'black'
+                    , 'border-style' :'solid'
+                    , 'border-width': '1px'
+                    , 'border-collapse':'collapse'
+                    })
+                .format({
+                    'Has Candidate': '{:.0f}'
+                    , 'Percentage with Candidate': '{:.0%}'
+                    })
+                .bar(subset=['Percentage with Candidate'], color=bar_color, vmin=0, vmax=1)
+                
+                .hide_index()
+                .render()
+            )
+        # smd_html = smd_count.to_html(index=False)
+        
+        return smd_html
+
+
+    def candidate_status_count(self):
+
+        candidates = pd.read_csv('data/candidates.csv')
+
+        status_count = candidates.groupby(['display_order', 'candidate_status']).size()
+        status_count.loc[('Total', 'Total')] = len(candidates)
+        
+        status_count_df = pd.DataFrame(status_count, columns=['Count']).reset_index()
+        status_count_df.rename(columns={'candidate_status': 'Candidate Status'}, inplace=True)
+
+        # status_html = status_count_df[['Candidate Status', 'Count']].to_html(index=False, justify='left')
+        
+        status_html = (
+            status_count_df[['Candidate Status', 'Count']].style
+                .set_properties(**{
+                    'border-color': 'black'
+                    , 'border-style' :'solid'
+                    , 'border-width': '1px'
+                    , 'border-collapse':'collapse'
+                    })
+                .hide_index()
+                .render()
+            )
+
+        return status_html
+
+
+    def run(self):
+
+        # smd_candidate_count('anc_id')
+        # smd_candidate_count('ward')
+        
+        candidate_status_count()
