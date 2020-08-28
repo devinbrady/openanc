cb3d393e701732cd1f29b820b62d8772904076e7
Devin Brady <devinbrady@gmail.com>
2020-08-28 13:57:09 -0400

    Aug 27 update from DCBOE

--- process_dcboe.py
+++ process_dcboe.py
@@ -19,8 +19,8 @@ def clean_csv():
     Result is a CSV of current candidates
     """
 
-    excel_file = 'dcboe-2020-08-22.xlsx'
-    dcboe_updated_at = '2020-08-22 12:49'
+    excel_file = 'dcboe-2020-08-27.xlsx'
+    dcboe_updated_at = '2020-08-27 16:53'
     print('Reading Excel file: ' + excel_file)
 
     df = pd.read_excel('data/dcboe/excel-clean/' + excel_file)
