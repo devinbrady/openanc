d23cb6d7a21c8c9368fe9338bdb345ca6b2b5d8f
Devin Brady <devinbrady@gmail.com>
2022-06-28 16:19:17 -0600

    Add redistricting maps

--- maps/find_points_for_labels.py
+++ maps/find_points_for_labels.py
@@ -11,20 +11,36 @@ import geopandas as gpd
 from polylabel import polylabel
 
 
-smd = gpd.read_file('smd.geojson')
+smd = gpd.read_file('to-mapbox-2022-smd-data.geojson')
 
 label_points = pd.DataFrame(columns=['smd_id', 'lon', 'lat'])
 
 for i, row in smd.iterrows():
-    district_outline = np.dstack(row.geometry.boundary[0].coords.xy).tolist()
+    print(row.smd_id)
+    district_outline = np.dstack(row.geometry.boundary.coords.xy).tolist()
     
     [lon, lat] = polylabel(district_outline)
     
-    label_points.loc[i, 'smd_id'] = 'smd_' + row['SMD_ID']
-    label_points.loc[i, 'smd'] = row['SMD_ID']
+    label_points.loc[i, 'smd_id'] = row['smd_id']
+    label_points.loc[i, 'smd'] = row['smd_id'].replace('smd_', '')
     label_points.loc[i, 'lon'] = lon
     label_points.loc[i, 'lat'] = lat
 
 
+# Specify the column names that will be in the final label-points CSV sent to Mapbox
+output_columns = [
+    'smd_id'
+    , 'lon'
+    , 'lat'
+    , 'smd'
+    , 'current_commissioner'
+    , 'commissioner_elect'
+    , 'map_display_box'
+]
+
+for c in output_columns:
+    if c not in label_points.columns:
+        label_points[c] = None
+
 label_points.sort_values(by='smd_id', inplace=True)
-label_points.to_csv('label_points.csv', index=False)
+label_points[output_columns].to_csv('label-points-2022.csv', index=False)
