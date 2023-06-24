d39ed3b179c8fc89c4540c3f301488356ba1fba5
Devin Brady <devinbrady@gmail.com>
2023-06-24 09:21:12 -0600

    Pull list from OANC

--- config.py
+++ config.py
@@ -1,12 +1,23 @@
+"""
+-----------------------
+Config File Instuctions
+-----------------------
+
+This file contains configuration variables for the entire OpenANC project. 
+
+Any script can use these variables by calling `import config` then
+referencing the variable, such as `config.current_redistricting_year`
+"""
+
 
 """
 current_election_year: This is the year when the current election is happening.
 
 When you set this to a future election, it has the effect of hiding candidates from the lists
-of SMDs (like on openanc.org/index.html). So if it's the year 2023 and this value is set to 2024,
+of SMDs (like on openanc.org/list.html). So if it's the year 2023 and this value is set to 2024,
 no candidates are displayed on these lists because no candidates yet exist in the candidates.csv
 table for the 2024 election.
-""" 
+"""
 current_election_year = 2024
 
 
