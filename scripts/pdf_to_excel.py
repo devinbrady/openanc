be40ba3162f00af0984e3445561b80fa0e1c7c08
Devin Brady <devinbrady@gmail.com>
2024-08-01 16:03:48 -0600

    Commissioner replacement

--- scripts/pdf_to_excel.py
+++ scripts/pdf_to_excel.py
@@ -5,8 +5,8 @@ import tabula
 import pandas as pd
 from pathlib import Path
 
-input_pdf = Path('~/Dropbox/OpenANC/DCBOE candidate lists 2024/General-24-ANC-Candidates-07312024.pdf')
-output_xlsx = Path('data/dcboe/excel-clean/dcboe-ballot-2024-07-31.xlsx')
+input_pdf = Path('~/Dropbox/OpenANC/DCBOE candidate lists 2024/General-24-ANC-Candidates-08012024.pdf')
+output_xlsx = Path('data/dcboe/excel-clean/dcboe-ballot-2024-08-01.xlsx')
 
 print(f'input: {input_pdf.name}')
 
@@ -19,5 +19,6 @@ print(f'Number of candidates: {df.Name.notnull().sum()}')
 columns_to_excel = ['ANC-SMD', 'Name', 'Date of Pick-up', 'Date Filed']
 
 df[columns_to_excel].to_excel(output_xlsx, index=False)
+# df.to_excel(output_xlsx, index=False)
 
 print(f'output: {output_xlsx.name}')
