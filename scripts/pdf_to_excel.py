b0aca9ee5f92804f7d2064ecce2d69579c22656d
Devin Brady <devinbrady@gmail.com>
2024-11-05 13:24:21 -0700

    Election day write-in

--- scripts/pdf_to_excel.py
+++ scripts/pdf_to_excel.py
@@ -5,47 +5,54 @@ import tabula
 import pandas as pd
 from pathlib import Path
 
-input_pdf = Path('~/Dropbox/OpenANC/DCBOE candidate lists 2024/General-24-ANC-Candidates-09242024.pdf')
-output_xlsx = Path('data/dcboe/excel-clean/dcboe-ballot-2024-09-24.xlsx')
+# input_pdf = Path('~/Dropbox/OpenANC/DCBOE candidate lists 2024/General-24-ANC-Candidates-09242024.pdf')
+# output_xlsx = Path('data/dcboe/excel-clean/dcboe-ballot-2024-09-24.xlsx')
 
-print(f'input: {input_pdf.name}')
+# print(f'input: {input_pdf.name}')
 
-dfs = tabula.read_pdf(input_pdf, pages='all')
+# dfs = tabula.read_pdf(input_pdf, pages='all')
 
-df = pd.concat(dfs)
+# df = pd.concat(dfs)
 
-print(f'Number of ballot candidates: {df.Name.notnull().sum()}')
+# print(f'Number of ballot candidates: {df.Name.notnull().sum()}')
 
-df['Date of Withdrawal'] = df.Name.str.extract("\(withdrew(.*)\)")
-df['Name'] = df.Name.str.replace("\(withdrew(.*)\)", '', regex=True, case=False).str.strip()
+# df['Date of Withdrawal'] = df.Name.str.extract("\(withdrew(.*)\)")
+# df['Name'] = df.Name.str.replace("\(withdrew(.*)\)", '', regex=True, case=False).str.strip()
 
-df['Challenge Notes'] = 'Challenge' + df.Name.str.extract("\(Challenge(.*)\)")
-df['Name'] = df.Name.str.replace("\(Challenge(.*)\)", '', regex=True, case=False).str.strip()
+# df['Challenge Notes'] = 'Challenge' + df.Name.str.extract("\(Challenge(.*)\)")
+# df['Name'] = df.Name.str.replace("\(Challenge(.*)\)", '', regex=True, case=False).str.strip()
 
-# Remove the rows where the candidate is "Write-In, If Any"
-df = df.loc[df['Name'].str.lower() != 'write-in, if any'].copy()
+# # Remove the rows where the candidate is "Write-In, If Any"
+# df = df.loc[df['Name'].str.lower() != 'write-in, if any'].copy()
 
-columns_to_excel = [
-    'ANC-SMD'
-    , 'Name'
-    , 'Date of Pick-up'
-    , 'Date Filed'
-    , 'Date of Withdrawal'
-    , 'Challenge Notes'
-    ]
+# columns_to_excel = [
+#     'ANC-SMD'
+#     , 'Name'
+#     , 'Date of Pick-up'
+#     , 'Date Filed'
+#     , 'Date of Withdrawal'
+#     , 'Challenge Notes'
+#     ]
 
-df[columns_to_excel].to_excel(output_xlsx, index=False)
+# df[columns_to_excel].to_excel(output_xlsx, index=False)
 
-print(f'output: {output_xlsx.name}')
+# print(f'output: {output_xlsx.name}')
 
 
 
-wu_input_pdf = Path('~/Dropbox/OpenANC/DCBOE candidate lists 2024/General-24-Write-in-Candidates-11012024.pdf')
-wu_output_xlsx = Path('data/dcboe/excel-clean/dcboe-write-in-2024-11-01.xlsx')
+wu_input_pdf = Path('~/Dropbox/OpenANC/DCBOE candidate lists 2024/General-24-Write-in-Candidates-11052024.pdf')
+wu_output_xlsx = Path('data/dcboe/excel-clean/dcboe-write-in-2024-11-05.xlsx')
 
 print(f'\ninput: {wu_input_pdf.name}')
 
 wu_dfs = tabula.read_pdf(wu_input_pdf, pages='all')
+
+for idx, _ in enumerate(wu_dfs):
+    if idx > 0:
+        wu_dfs[idx].loc[len(wu_dfs[idx])+1] = wu_dfs[idx].columns
+        wu_dfs[idx].columns = ["Write-in Candidate's Name", "Office", "Party", "Date Form Filed"]
+        print(wu_dfs[idx])
+
 
 wu_df = pd.concat(wu_dfs)
 
